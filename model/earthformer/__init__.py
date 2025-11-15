2a2f0377200467b789f345bc2ccffe58b00caa37
iamy20896-art <iamy20896@gmail.com>
2025-11-15 18:22:47 +0800

    Add files via upload

---
+++ model/earthformer/__init__.py
@@ -0,0 +1,4 @@
+from .cuboid_transformer import CuboidTransformerModel
+from .cuboid_transformer_patterns import CuboidSelfAttentionPatterns, CuboidCrossAttentionPatterns
+
+__all__ = ['CuboidTransformerModel', 'CuboidSelfAttentionPatterns', 'CuboidCrossAttentionPatterns']
